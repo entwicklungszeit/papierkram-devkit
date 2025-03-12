d7ef62d3d4e935c5fb72f0fbeaddac467b204c9a
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-03-12 20:41:59 +0100

    test(toggl): create e2e test for http post endpoint

---
+++ apps/papierkram-devkit-api-e2e/src/papierkram-devkit-api/import-current-week.spec.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { endOfWeek, startOfWeek } from 'date-fns'
+
+describe('Import time entries form toggl', () => {
+  it('yields created if an import ran successful', async () => {
+    const monday = 1
+    const thisWeek = {
+      from: startOfWeek(new Date(), { weekStartsOn: monday }),
+      to: endOfWeek(new Date(), { weekStartsOn: monday })
+    }
+
+    const response = await axios.post(
+      `http://localhost:3000/api/imports/toggl`,
+      thisWeek,
+      { headers: { 'Content-Type': 'application/json' } }
+    )
+
+    expect(response.status).toBe(201)
+  })
+})
