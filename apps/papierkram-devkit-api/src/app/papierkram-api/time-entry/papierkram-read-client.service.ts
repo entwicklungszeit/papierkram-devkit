31c7ccb086cb8f87af20bf8a6c92121d23b0ff78
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-03-13 13:08:14 +0100

    ref: use papierkramApiConfig

--- apps/papierkram-devkit-api/src/app/papierkram-api/time-entry/papierkram-read-client.service.ts
+++ apps/papierkram-devkit-api/src/app/papierkram-api/time-entry/papierkram-read-client.service.ts
@@ -1,31 +1,20 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { HttpService } from '@nestjs/axios'
-import { ConfigService } from '@nestjs/config'
 
 import { PapierkramTimeEntry } from '../types/papierkram-time-entry'
+import { PapierkramApiConfig } from './papierkram-api-config.service'
 import { TimeFrame } from '../../utils/time-frame'
 import { toDateOnly } from '../../utils/to-date-only'
 
 @Injectable()
 export class PapierkramTimeEntryReadClient {
   private logger = new Logger('PapierkramTimeEntryReadClient')
-  private options = {
-    apiUrl: '',
-    apiToken: '',
-    projectId: '',
-    userId: '',
-    taskId: ''
-  }
 
-  constructor(private config: ConfigService, private httpClient: HttpService) {
-    this.options.apiUrl = this.config.get<string>('papierkram_api_url') ?? ''
-    this.options.apiToken =
-      this.config.get<string>('papierkram_api_token') ?? ''
-    this.options.projectId =
-      this.config.get<string>('papierkram_project_id') ?? ''
-    this.options.userId = this.config.get<string>('papierkram_user_id') ?? ''
-    this.options.taskId = this.config.get<string>('papierkram_task_id') ?? ''
-  }
+  constructor(
+    private httpClient: HttpService,
+    private config: PapierkramApiConfig
+  ) {}
+
   async readTimeEntries(timeFrame: TimeFrame): Promise<PapierkramTimeEntry[]> {
     const { from, to } = toDateOnly(timeFrame)
 
@@ -34,11 +23,11 @@ export class PapierkramTimeEntryReadClient {
         entries: PapierkramTimeEntry[]
         hasMore: boolean
       }>(
-        `${this.options.apiUrl}/tracker/time_entries?project_id=${this.options.projectId}&start_time_range_start=${from}&start_time_range_end=${to}`,
+        `${this.config.apiUrl}/tracker/time_entries?project_id=${this.config.projectId}&start_time_range_start=${from}&start_time_range_end=${to}`,
         {
           headers: {
             Accept: 'application/json', // without papierkram-api yields 406 Not Acceptable
-            Authorization: `Bearer ${this.options.apiToken}`
+            Authorization: `Bearer ${this.config.apiToken}`
           }
         }
       )
